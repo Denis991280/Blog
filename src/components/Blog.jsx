5c3b4f90a2aa41f986bde236d3930d2d932dc375
unknown <denis.hadzipasic@partyrent.com>
2023-10-16 22:00:46 +0200

    more changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import { isValidElement, useState } from "react"
 
 export default function Blog() {
+    const posted = new Date();
     const [blogTitle, setBlogTitle] = useState("");
     const [name, setName] = useState("");
     const [image, setImage] = useState("");
@@ -11,28 +12,56 @@ export default function Blog() {
         setBlogTitle(e.target.value)
     }
 
+    const nameInput = (e) => {
+        setName(e.target.value)
+    }
 
+    const imageInput = (e) => {
+        setImage(e.target.value)
+    }
+
+    const contentInput = (e) => {
+        setContent(e.target.value)
+    }
 
     const submitBlog = () => {
-        const createBlog = {id: new Date().getTime(), title, name, img, content}
+        const createBlog = {id: new Date().getTime(), blogTitle, name, image, content}
 
-        setBlogs =([...blogs, createBlog])
-        setBlogTitle(blogTitle)
+        setBlogs([...blogs, createBlog])
     }
 
     return (
+        <>
         <div className="formContainer">
             <h2>Post your blog!</h2>
-
-            {/* <form className="blogForm"> */}
+            <div className="blogForm">
                 <input onChange={titleInput} type="text" placeholder="Name the title of your blog" />
-                <input type="text" placeholder="Enter your name" />
-                <input type="text" placeholder="URL of the img" />
-                <textarea placeholder="Enter the content of your blog:"></textarea>
+                <input onChange={nameInput} type="text" placeholder="Enter your name" />
+                <input onChange={imageInput} type="text" placeholder="URL of the img" />
+                <textarea onChange={contentInput} placeholder="Enter the content of your blog:"></textarea>
                 <button onClick={submitBlog}>Submit</button>
-            {/* </form> */}
+            </div>
 
-
+            <div className="previewContainer">
+                <h3>Blog preview</h3>
+                <p>{blogTitle}</p>
+                <p>{name}</p>
+                <p>{content}</p>
+            </div>
         </div>
+
+        {blogs.map((blog) => {
+            return (
+                <div key={blog.id}>
+                    <p>{blog.blogTitle}</p>
+                    <p>Posted on: {(posted.getMonth() + 1) + "/" + posted.getDate() + " " + posted.getHours() + ":" + posted.getMinutes()}</p>
+                    <p>{blog.name}</p>
+                    <img src={blog.image} height={100}/>
+                    <p>{blog.content}</p>
+                    <button onClick={(e) => {const deleteBlogs = blogs.filter((deleteSingleBlog) => {return deleteSingleBlog.id !== blog.id;}); setBlogs(deleteBlogs);}}>Delete</button>
+                </div>
+            )
+        })}
+        </>
     )
 }
